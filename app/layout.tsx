34af428b53ce3bbd808666a10c6536844e68d120
Atilola Oyediji <atilola.oyediji@gmail.com>
2024-12-15 22:20:37 +0100

    completed project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,14 @@
+import { Metadata } from 'next'
 import '@/app/ui/global.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({
   children,
