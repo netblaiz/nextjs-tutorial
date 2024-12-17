b1e2af697eb434c5b953a548a444eecab8242e1c
Atilola Oyediji <atilola.oyediji@gmail.com>
2024-12-17 09:41:01 +0100

    invoice form removed

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useActionState } from 'react'
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
+import { CustomerField } from '@/app/lib/definitions'
 import Link from 'next/link'
 import {
   CheckIcon,
